c2e6e92a2e2b6167fe6e9a1c762a52b6dae99f2d
ZIAD AHMED <104504486+ziadAhmedFawzy@users.noreply.github.com>
2024-02-12 04:56:13 +0200

    add function do  repeat key with length txt

---
+++ vigenere_encryption/enc.js
@@ -0,0 +1,31 @@
+let word = document.getElementById("word");
+let keys = document.getElementById("keys");
+let enc = document.getElementById("enc");
+let dec = document.getElementById("dec");
+let clear = document.getElementById("clear");
+
+// this is function to repeat key with length txt
+function keysList(txt,repeat) {
+    sum = 0;
+    for(let i = 0; i < txt.length; i++)
+    {   
+        for(let x = 0; x < repeat.length; x++)
+        {
+            if(repeat[x] == undefined)
+            {
+                continue;
+            }
+            sum = sum + repeat[x]
+        }
+    }
+    return sum.slice(1, txt.length + 1)
+}
+
+enc.addEventListener("click", function() {
+    let num = 0
+    for(let i = 0; i < word.value.length; i++)
+    {
+        num = +keysList(word.value, keys.value)[i] + word.value[i].charCodeAt(0)
+        console.log(String.fromCharCode(num))
+    }
+})
